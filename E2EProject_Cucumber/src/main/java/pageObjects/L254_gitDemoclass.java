124f15caa2b3757ed7375eb8a28697b2075e68c3
Akshat <ahuja.akshat1@gmail.com>
2022-08-30 13:40:21 +0530

    changes made by X

--- E2EProject_Cucumber/src/main/java/pageObjects/L254_gitDemoclass.java
+++ E2EProject_Cucumber/src/main/java/pageObjects/L254_gitDemoclass.java
@@ -5,7 +5,10 @@ public class L254_gitDemoclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello, I am a change!");	
+		System.out.println("Hello, I am a change!");
+		System.out.println("Hello, I am a changed by X!");	
+		System.out.println("Hello, Today is a good day");	
+
 		
 	}
 
---
+++ E2EProject_Cucumber/src/main/java/pageObjects/L255gitDemo2.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+public class L255gitDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Adding new class");
+		
+	}
+
+}
