77c732599a5dbe6e36819179069c0126d6ee7be3
Akshat <ahuja.akshat1@gmail.com>
2022-08-31 23:50:55 +0530

    Architect 2's changes to GitX framework for develop branch

---
+++ E2EProject_Cucumber/src/main/java/pageObjects/L256_Branching2.java
@@ -0,0 +1,12 @@
+package pageObjects;
+
+public class L256_Branching2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Architect 2 made changes to branch GitX");
+		
+	}
+
+}
